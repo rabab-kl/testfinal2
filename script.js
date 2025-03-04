1fe0adffec28af47cdd081504b118c33640e1800
rabab-kl <Rabab.KOULI@ecole-it.com>
2025-03-04 15:02:10 +0100

    2

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    $('.carousel').carousel({
+        interval: 3000,
+        pause: 'hover'
+    });
+
+    let cards = document.querySelectorAll(".card");
+    cards.forEach(card => {
+        card.addEventListener("mouseenter", function() {
+            this.style.boxShadow = "0 0 15px rgba(255, 60, 0, 0.8)";
+        });
+        card.addEventListener("mouseleave", function() {
+            this.style.boxShadow = "none";
+        });
+        const slider = document.querySelector(".carousel-item");
+        const leftBtn = document.querySelector(".left-btn");
+        const rightBtn = document.querySelector(".right-btn");
+        const scrollAmount = 220;
+        leftBtn.addEventListener("click", function() {
+            slider.scrollBy({
+                left: -scrollAmount,
+                behavior: "smooth"
+            });
+        });
+        rightBtn.addEventListener("click", function() {
+            slider.scrollBy({
+                left: scrollAmount,
+                behavior: "smooth"
+            });
+        });
+    });
+});
